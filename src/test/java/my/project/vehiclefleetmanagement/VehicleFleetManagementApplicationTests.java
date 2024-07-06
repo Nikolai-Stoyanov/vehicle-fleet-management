4056d510f9463e036f101a72eb1bb3728e481d1d
N10394 <NikolayAntonov.Stoyanov@energo-pro.bg>
2024-07-06 16:17:43 +0300

    initial commit

---
+++ src/test/java/my/project/vehiclefleetmanagement/VehicleFleetManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package my.project.vehiclefleetmanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VehicleFleetManagementApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
