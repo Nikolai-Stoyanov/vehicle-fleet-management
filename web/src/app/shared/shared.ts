d1b19d6c1d07c7152c7ff9d103ba1ddb9d065b7f
N10394 <NikolayAntonov.Stoyanov@energo-pro.bg>
2024-07-07 14:55:43 +0300

    add frontend

---
+++ web/src/app/shared/shared.ts
@@ -0,0 +1,21 @@
+import { InjectionToken } from '@angular/core';
+
+export const ENVIRONMENT = new InjectionToken<any>('ENVIRONMENT');
+
+export interface ServiceUrl {
+  baseUrl: string;
+}
+
+export interface EnvironmentInfo {
+  name: string;
+}
+
+export interface RuntimeConfiguration {
+  backend: ServiceUrl;
+  environment: EnvironmentInfo;
+  appName: string;
+  appVersion: string;
+  buildId: string;
+  buildDate: string;
+  debugIsEnabled?: boolean;
+}
