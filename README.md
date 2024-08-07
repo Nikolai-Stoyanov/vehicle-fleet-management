d4435005ab13860998e2846e466a401c5894443b
N10394 <NikolayAntonov.Stoyanov@energo-pro.bg>
2024-08-07 15:09:40 +0300

    add readme file; some small changes;

--- README.md
+++ README.md
@@ -1,1 +1,54 @@
-# vehicle-fleet-management
+
+# Vehicle fleet managment
+
+The project is for fleet management. Car makes and models, fuel types and gas stations can be added. Car files can be created, and monthly declarations for distance traveled and fuel filled can be created based on them.
+The project was created for the Spring Advanced course in Softuni.
+
+
+## Authors
+
+- [@Nikolai-Stoyanov](https://github.com/Nikolai-Stoyanov)
+
+
+## Requirements
+
+The list of tools required to build and run the project:
+
+```bash
+Open JDK 17
+npm 10.5.2
+Angular CLI 18
+Node 20.14.0
+````
+## Run Locally UI
+
+Clone the project
+
+```bash
+  git clone https://github.com/Nikolai-Stoyanov/vehicle-fleet-management
+```
+
+Go to the project directory
+
+```bash
+  cd vehicle-fleet-management/web
+```
+
+Install dependencies
+
+```bash
+  npm install
+```
+
+Start the server
+
+```bash
+  npm start
+```
+
+Start the server bg translation
+
+```bash
+  npm run start:bg
+```
+
--- src/main/java/my/project/vehiclefleetmanagement/exceptions/RestExceptionHandler.java
+++ src/main/java/my/project/vehiclefleetmanagement/exceptions/RestExceptionHandler.java
@@ -1,8 +1,11 @@
 package my.project.vehiclefleetmanagement.exceptions;
 
 
+import my.project.vehiclefleetmanagement.config.Messages;
 import my.project.vehiclefleetmanagement.model.dtos.user.ErrorDto;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -16,5 +19,13 @@ public class RestExceptionHandler {
         return ResponseEntity
                 .status(ex.getStatus())
                 .body(new ErrorDto(ex.getMessage()));
+    }
+
+    @ExceptionHandler(value = { MethodArgumentNotValidException.class })
+    @ResponseBody
+    public ResponseEntity<ErrorDto> handleMethodArgumentNotValidException(MethodArgumentNotValidException ex) {
+        return ResponseEntity
+                .status(HttpStatus.BAD_REQUEST)
+                .body(new ErrorDto(Messages.getLocaleMessage("Validation_Failed")));
     }
 }
